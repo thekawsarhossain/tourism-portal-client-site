c0a594d03d29670ee5e2d17199e3e776fdd299da
Kawsar-Hossain-318 <thekawsarhossain@gmail.com>
2021-11-01 23:39:18 +0600

    ffinishinh touch

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyCfzc0MEEVdeZl2DwO0Y8fe2WBLb8KzpvU",
+    authDomain: "fir-practice-b7d83.firebaseapp.com",
+    projectId: "fir-practice-b7d83",
+    storageBucket: "fir-practice-b7d83.appspot.com",
+    messagingSenderId: "500526914512",
+    appId: "1:500526914512:web:7c91b27fdacd4a2b3677d5"
 };
 
 export default firebaseConfig;
